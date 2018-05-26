fadc9267184236f169b14da867365a90788bc440
vagrant <vagrant@localhost.localdomain>
2018-05-26 17:38:41 +0000

    Liverpool

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Go Reds!')
 })
 
 app.listen(app.get('port'), function() {
