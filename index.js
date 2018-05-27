e27b231b6ae16934cbcbf8912484bc47d9cc36b3
vagrant <vagrant@localhost.localdomain>
2018-05-27 07:58:03 +0000

    Removed semicolon

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(request, response) {
   response.send('Go Reds!');
 });
 
-app.listen(app.get('port'), function() {;
+app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
