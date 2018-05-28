8e57a334f1f54294519d72cb2f811a951212361e
Travis CI <radu.domnu@gmail.com>
2018-05-28 17:38:24 +0000

    New deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('What about the Ramos a44**** :(');
+  response.send('Ramos is the culprit as usual');
 });
 
 app.listen(app.get('port'), function() {
