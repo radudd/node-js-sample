6af29f5b68917822270db3f0f72411b0f23dca79
Travis CI <vagrant@localhost.localdomain>
2018-05-28 09:49:58 +0000

    Removed git commit from terraform

---
+++ terraform/deploy_aws_host.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Apply Terraform deployment
+TF_DIR=`cd $(dirname $0); pwd`
+cd $TF_DIR
+terraform plan || exit 99
+terraform apply -auto-approve || exit 99
+
+## Commit changes
+#echo "Git: Configuring git user.name..." && git config user.name "$GIT_USERNAME"
+#echo "Git: Setting remote repository..." && git remote add base "$GIT_REPO"
+#echo "Git: Checking out master" && git checkout master
+#echo "Git: Commiting changes to terraform state..." && git commit -am "`date +%Y%m%d-%H%MAdded`: Terraforming by Travis CI"
+#echo "Git: Pushing to remote repo..." && git push base master
