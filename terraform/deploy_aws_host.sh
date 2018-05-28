65816c1ed576dc525cf841368241621e9f10ddd2
Travis CI <vagrant@localhost.localdomain>
2018-05-28 15:28:41 +0000

    Updated terraform deploy - added commit of state files to GIT

--- terraform/deploy_aws_host.sh
+++ terraform/deploy_aws_host.sh
@@ -8,8 +8,9 @@ terraform plan || exit 99
 terraform apply -auto-approve || exit 99
 
 ## Commit changes
-echo "Git: Configuring git user.name..." && git config user.name "$GIT_USERNAME"
-echo "Git: Setting remote repository..." && git remote add base "$GIT_REPO"
-echo "Git: Checking out master" && git checkout master
-echo "Git: Commiting changes to terraform state..." && git commit -am "`date +%Y%m%d-%H%M`: Terraforming by Travis CI"
-echo "Git: Pushing to remote repo..." && git push base master
+#echo "Git: Configuring git user.name..." && echo "git config user.name $GIT_USERNAME" | tee /dev/stderr | bash
+#echo "Git: Setting remote repository..." && git remote add base "$GIT_REPO" | tee /dev/stderr | bash
+#echo "Git: Checking out master" && git checkout master | tee /dev/stderr | bash
+echo "Git: Commiting changes to terraform state..." && git commit -am "`date +%Y%m%d-%H%M`: Terraforming by Travis CI" | tee /dev/stderr | bash
+echo "Git: Pushing to remote repo..." && git push origin master | tee /dev/stderr | bash
+#echo "Git: Pushing to remote repo..." && git push base master | tee /dev/stderr | bash
