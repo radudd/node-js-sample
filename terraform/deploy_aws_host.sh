2d181fb20081cd5b1b2474c55abf1fe735b7f2fc
Travis CI <radu.domnu@gmail.com>
2018-05-28 20:35:27 +0000

    Try to remove after_deploy and integrate it in the deploy script

--- terraform/deploy_aws_host.sh
+++ terraform/deploy_aws_host.sh
@@ -13,3 +13,11 @@ echo -e "[Git]: Checking out master" && echo "git checkout master" | tee /dev/st
 echo -e "[Git]: Add to staging..." && echo "git add ." | tee /dev/stderr | bash
 echo -e "[Git]: Commiting changes ..." && echo "git commit -am \"`date +%Y%m%d-%H%M`: Terraforming by Travis CI\"" | tee /dev/stderr | bash
 echo -e "[Git]: Pushing to remote repo..." && echo "git push base master" | tee /dev/stderr | bash
+
+# Deploy App to Dokku
+export dokku_host=`terraform output public_dns`
+ssh-keyscan ${dokku_host} >> ~/.ssh/known_hosts
+git remote add dokku dokku@${dokku_host}:node-js-app
+git fetch --unshallow
+until ssh -q -o StrictHostKeyChecking=no ${dokku_host} -l dokku ssh-keys:list; do sleep 30; done; git push dokku master
+echo http://${dokku_host}:5000
