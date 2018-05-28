6f79df03e8d2fea8ca105b4fc1b90b9b4eafc3ab
Travis CI <radu.domnu@gmail.com>
2018-05-28 17:33:15 +0000

    New deploy

--- terraform/deploy_aws_host.sh
+++ terraform/deploy_aws_host.sh
@@ -10,6 +10,6 @@ terraform apply -auto-approve || exit 99
 echo "Git: Configuring git user.name..." && echo -e "git config user.name $GIT_USERNAME" | tee /dev/stderr | bash
 echo "Git: Setting remote repository..." && echo -e "git remote add base $GIT_REPO" | tee /dev/stderr | bash
 #echo "Git: Checking out master" && echo -e "git checkout master" | tee /dev/stderr | bash
-echo "Git: Add to staging..." && echo "git add -A" | tee /dev/stderr | bash
+echo "Git: Add to staging..." && echo "git add ." | tee /dev/stderr | bash
 echo "Git: Commiting changes ..." && echo "git commit -am \"`date +%Y%m%d-%H%M`: Terraforming by Travis CI\"" | tee /dev/stderr | bash
 echo "Git: Pushing to remote repo..." && echo "git push base master" | tee /dev/stderr | bash
