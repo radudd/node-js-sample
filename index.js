8e4f68d211a69d1ba833bf47c6e6f2677d62b83d
Travis CI <radu.domnu@gmail.com>
2018-05-28 18:50:10 +0000

    New infra

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Oana nu e atenta la mine si sta toata ziua pe telefon! :P');
+  response.send('Radu nu ii va da telefonul Oanei :P');
 });
 
 app.listen(app.get('port'), function() {
